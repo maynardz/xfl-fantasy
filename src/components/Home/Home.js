be6e955a648d193e4aacfb174f9e85439d163706
ZSM <maynard.zach23@gmail.com>
2023-01-29 15:28:02 -0500

    homepage world map

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 // import wolfpack_logo from '../../assets/images/wolfpack_logo_2.png';
 import wolfpack_pictoral from '../../assets/images/wolfpack_pictoral.png';
 
-import { Card, CardBody, Main, Heading, Paragraph, Button } from 'grommet';
+import { Card, CardBody, Heading, Paragraph, Button, WorldMap, Tip } from 'grommet';
 
 // import {
 //   useNavigate
@@ -23,9 +23,67 @@ const Dashboard = props => {
           {/* <Header background="#fcee09" style={{ padding: '1em', fontWeight: '700' }}>
             LEAGUES
           </Header> */}
-          <Main pad="large" background={'white'}>
-            <img id='wolfpack_logo_dash' src={wolfpack_pictoral} alt='Wolfpack Fantasy Logo featuring a prominent W and P.' />
-          </Main>
+          {/* <Main pad="large" background={'white'}> */}
+            {/* <img id='wolfpack_logo_dash' src={wolfpack_pictoral} alt='Wolfpack Fantasy Logo featuring a prominent W and P.' /> */}
+            <div style={{display: 'block', margin: '0 auto'}}>
+              <WorldMap
+                color="#aaaaaa"
+                places={[
+                  {
+                    name: 'Arlington Renegades',
+                    location: [32.705002, -97.122780],
+                    color: '#6ab3e6',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/arlington', '_blank') },
+                    onHover: (name) => { return name ? <p>Arlington Renegades</p> : <div></div> }
+                  },
+                  {
+                    name: 'D.C. Defenders',
+                    location: [38.89511, -77.03637],
+                    color: '#c80f2e',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/washington-dc', '_blank') },
+                  },
+                  {
+                    name: 'Houston Roughnecks',
+                    location: [29.749907, -95.358421],
+                    color: '#c10130',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/houston', '_blank') },
+                  },
+                  {
+                    name: 'Orlando Guardians',
+                    location: [28.538336, -81.379234],
+                    color: '#acfb09',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/orlando', '_blank') },
+                  },
+                  {
+                    name: 'San Antonio Brahmas',
+                    location: [29.424349, -98.491142],
+                    color: '#fee202',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/san-antonio', '_blank') },
+                  },
+                  {
+                    name: 'Seattle Sea Dragons',
+                    location: [47.608013, -122.335167],
+                    color: '#00843d',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/seattle', '_blank') },
+                  },
+                  {
+                    name: 'St. Louis Battlehawks',
+                    location: [38.627003, -90.199402],
+                    color: '#002677',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/st-louis', '_blank') },
+                  },
+                  {
+                    name: 'Vegas Vipers',
+                    location: [36.114647, -115.172813],
+                    color: '#ff0600',
+                    onClick: (name) => { window.open('https://www.xfl.com/teams/las-vegas', '_blank') },
+                  },
+                ]}
+                selectColor="brand"
+                style={{height: 'auto', width: '80vw', marginTop: '3em', marginBottom: '-1em'}}
+              />
+            </div>
+          {/* </Main> */}
           <CardBody pad="large">
             <div>
               <Heading>Welcome to Wolfpack Fantasy</Heading>
--- src/components/Profile/DisplayLeague/LeagueTeams/Invite/Invite.js
+++ src/components/Profile/DisplayLeague/LeagueTeams/Invite/Invite.js
@@ -7,7 +7,7 @@ const Invite = props => {
   return (
     <div>
       <Box align="start">
-        <Button primary onClick={() => props.setOpen(!props.open)} label="+ INVITE FRIENDS TO JOIN" style={{display: 'block', margin: '0 auto'}} />
+        <Button primary onClick={() => props.setOpen(!props.open)} label="＋ INVITE FRIENDS TO JOIN" style={{display: 'block', margin: '0 auto', backgroundColor: '#353200', border: '2px solid #fcee09', padding: '0.5em', color: '#fff', width: '80%'}} />
           <Collapsible open={props.open}>
             <Box
               background="light-2"
