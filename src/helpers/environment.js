5e6e043ff3c11eaecbd00c84f5be2f58b0592659
ZSM <maynard.zach23@gmail.com>
2023-02-19 18:46:31 -0500

    helpers

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'www.xfl-client.herokuapp.com':
+    case 'xfl-client.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://xfl-gqserver.herokuapp.com'
 }
